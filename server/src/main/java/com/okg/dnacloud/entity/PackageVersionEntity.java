25ed8aaa5ec25d75d4fad1b674f4d609dfab0548
chunyu.pan <chunyu.pan@okg.com>
2026-05-09 19:39:08 +0800

    v1

--- server/src/main/java/com/okg/dnacloud/entity/PackageVersionEntity.java
+++ server/src/main/java/com/okg/dnacloud/entity/PackageVersionEntity.java
@@ -45,7 +45,7 @@ public class PackageVersionEntity {
     @Column(name = "package_hash", nullable = false)
     private String packageHash;
 
-    @Column(name = "artifact_path", nullable = false)
+    @Column(name = "artifact_path", nullable = true)
     private String artifactPath;
 
     @Column(name = "manifest_json", columnDefinition = "TEXT")
